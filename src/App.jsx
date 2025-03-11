95f51c92fa5f2886e9f83ff3ebf887966bc22fba
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-03-11 05:39:02 +0200

    Add text styling to the main heading in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main className="max-w[19.063rem] md:max-w-[40.438rem] xl:max-w-[69.625rem] mx-auto">
-      <h1 className="">React App</h1>
+      <h1 className="text-preset-1">React App</h1>
     </main>
   );
 }
